386e6ce960dc70aee7e2f99124708803a66e52d3
傅长路 <fulu0709@foxmail.com>
2021-04-01 18:35:09 +0800

    feat: 绑定快捷键输入插件

---
+++ examples/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <hot-key-input
+      :hotkey.sync="hotKeyObj.keys"
+      :verify="handleHotkeyVerify"
+      :max="2"
+      :reset="true"
+      :shake="true"
+      placeholder="请按需要绑定的按键，支持组合按键"></hot-key-input>
+
+  <div style="margin: 20px;">
+    <button @click="getCurrentHotkeys">获取当前</button>
+  </div>      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      list:[
+      {
+        tag: 'save',
+        keys: ["Ctrl+A", "Alt+D"]
+      }
+      ],
+      hotKeyObj: {
+        tab: 'save',
+        keys: ["Ctrl+A", "Alt+D"]
+      },
+    }
+  },
+  methods:{
+    handleHotkeyVerify(hotkey) {
+      console.log('验证：', hotkey, this.hotKeyObj)
+      // for (const item of this.hotKeyList) {
+      // }
+      return true;
+    },
+    getCurrentHotkeys(){
+      console.log('获取当前：', this.hotKeyObj)
+    }
+  }
+}
+</script>
+
+<style>
+  @keyframes hot-key-input-shake {
+  0% {transform: scale(1);}
+  10%, 20% {transform: scale(0.9) rotate(-3deg);}
+  30%, 50%, 70%, 90% {transform: scale(1.1) rotate(3deg);}
+  40%, 60%, 80% {transform: scale(1.1) rotate(-3deg);}
+  100% {transform: scale(1) rotate(0);}
+}
+</style>
---
+++ examples/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// 导入组件库
+import HotKeyInput from './../lib/index.min.js'//'./../packages/index'
+// import HotKeyInput from './../packages/index'
+
+// 注册组件库
+Vue.use(HotKeyInput)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
